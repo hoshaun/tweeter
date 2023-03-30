dbb3cb166b524c0dd71debc8f869dd1278ba0e89
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-30 14:32:33 -0400

    fix some bugs with scrolling and box shadow

--- public/scripts/compose-button.js
+++ public/scripts/compose-button.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  const $composeButton = $('.compose-button');
+  const $composeButton = $('#compose-button');
   const $composeTextArea = $('#tweet-text');
 
   // scroll to top and focus text area on click
   $composeButton.on('click', function() {
     $('html, body').animate({
-      scrollTop: 0
+      scrollTop: $(".container").offset().top - 50
     }, 'slow');
     $composeTextArea.focus();
   });
