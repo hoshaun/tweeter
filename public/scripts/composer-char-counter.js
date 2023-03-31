ae35e90548c84a7738077b8f2e1ed88cbfb45aef
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-31 12:25:40 -0400

    refactor composer-char-counter.js to use jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-  const tweetText = document.getElementById('tweet-text');
-  const tweetCharCounter = document.getElementById('tweet-char-counter');
-  tweetText.addEventListener('input', function() {
+  const $tweetText = $('#tweet-text');
+  const $tweetCharCounter = $('#tweet-char-counter');
+  $tweetText.on('input', function() {
     const charLimit = 140;
     const tweetLength = this.value.length;
-    tweetCharCounter.value = charLimit - tweetLength;
-    tweetCharCounter.style.color = tweetCharCounter.value < 0 ? 'red' : 'black';
+    $tweetCharCounter.val(charLimit - tweetLength);
+    if ($tweetCharCounter.val() < 0) {
+      $tweetCharCounter.css('color', 'red');
+    } else {
+      $tweetCharCounter.css('color', 'black');
+    }
   });
 });
